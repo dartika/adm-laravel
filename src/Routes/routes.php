e39d80b9d00ea2b69ef2fa9248a1e7e639287311
mbellido <mbellido@dartika.com>
2017-07-20 08:49:12 +0200

    Update routes with global namespace and route name prefix

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -11,26 +11,24 @@
 |
 */
 
-$namespace = 'Dartika\Adm\Http\Controllers';
+Route::group(['namespace' => 'Dartika\Adm\Http\Controllers', 'prefix' => config('dartika-adm.prefix'), 'middleware' => 'web', 'as' => 'dartika-adm.'], function () {
+    Route::get('login', "Auth\LoginController@showLoginForm")->name('login');
+    Route::post('login', "Auth\LoginController@login");
+    Route::get('logout', "Auth\LoginController@logout")->name('logout');
 
-Route::group(['prefix' => config('dartika-adm.prefix'), 'middleware' => 'web'], function () use ($namespace) {
-    Route::get('login', "{$namespace}\Auth\LoginController@showLoginForm")->name('dartika-adm.login');
-    Route::post('login', "{$namespace}\Auth\LoginController@login");
-    Route::get('logout', "{$namespace}\Auth\LoginController@logout")->name('dartika-adm.logout');
-
-    Route::group(['middleware' => 'auth:adm'], function () use ($namespace) {
+    Route::group(['middleware' => 'auth:adm'], function () {
         Route::get('/', function () {
             return view('dartika-adm::sections.dashboard.index');
-        })->name('dartika-adm.dashboard');
+        })->name('dashboard');
 
-        Route::group(['prefix' => 'adm-users'], function () use ($namespace) {
-            Route::get('/', "{$namespace}\AdmUsersController@index")->name('dartika-adm.adm_users.index');
-            Route::get('/new', "{$namespace}\AdmUsersController@create")->name('dartika-adm.adm_users.create');
-            Route::post('/', "{$namespace}\AdmUsersController@store")->name('dartika-adm.adm_users.store');
-            Route::get('/{admUser}', "{$namespace}\AdmUsersController@edit")->name('dartika-adm.adm_users.edit');
-            Route::put('/{admUser}', "{$namespace}\AdmUsersController@update")->name('dartika-adm.adm_users.update');
-            Route::get('/{admUser}/delete', "{$namespace}\AdmUsersController@delete")->name('dartika-adm.adm_users.deleteget');
-            Route::delete('/{admUser}', "{$namespace}\AdmUsersController@delete")->name('dartika-adm.adm_users.delete');
+        Route::group(['prefix' => 'adm-users', 'as' => 'adm_users.'], function () {
+            Route::get('/', "AdmUsersController@index")->name('index');
+            Route::get('/new', "AdmUsersController@create")->name('create');
+            Route::post('/', "AdmUsersController@store")->name('store');
+            Route::get('/{admUser}', "AdmUsersController@edit")->name('edit');
+            Route::put('/{admUser}', "AdmUsersController@update")->name('update');
+            Route::get('/{admUser}/delete', "AdmUsersController@delete")->name('deleteget');
+            Route::delete('/{admUser}', "AdmUsersController@delete")->name('delete');
         });
     });
 });
