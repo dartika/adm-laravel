e6c06c870f33f014e3dfb4f68f5b47a933793b47
mbellido <mbellido@dartika.com>
2017-07-20 13:53:17 +0200

    Added sample section

---
+++ resources/views/sections/helloworld/index.blade.php
@@ -0,0 +1,14 @@
+@extends('dartika-adm::layouts.default')
+
+@section('title', 'Hello World!')
+
+@section('page_title', 'Hello World!')
+@section('page_description', 'Hello World!')
+
+@section('breadcrumb')
+    <li><a href="#"><i class="fa fa-hand-spock-o"></i> Hello World!</a></li>
+@endsection
+
+@section('content')
+    Hello World!
+@endsection
