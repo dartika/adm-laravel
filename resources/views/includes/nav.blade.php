b8e9ff2c2bd9f36f060907270f12d70c0ca79612
mbellido <mbellido@dartika.com>
2017-08-16 14:48:59 +0200

    Deleted stub route from nav view

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -7,8 +7,6 @@
             <li class="{{ Route::is('dartika-adm.dashboard') ? 'active' : ''}}"><a href="{{ route('dartika-adm.dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('dartika-adm::adm.dashboard') }}</span></a></li>
             
             <li class="{{ Route::is('dartika-adm.adm_users*') ? 'active' : ''}}"><a href="{{ route('dartika-adm.adm_users.index') }}"><i class="fa fa-users"></i> <span>{{ trans('dartika-adm::adm.admin_users') }}</span></a></li>
-
-            <li class="{{ Route::is('dartika-adm.helloworld') ? 'active' : ''}}"><a href="{{ route('dartika-adm.helloworld') }}"><i class="fa fa-hand-spock-o"></i> <span>Hello World!</span></a></li>
         </ul>
         <!-- /.sidebar-menu -->
     </section>
